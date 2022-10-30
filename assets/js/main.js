96cb1507c6339ee7e288eb1701811d883d3bdd46
Gabriel <gabriel.gracilianomb@gmail.com>
2022-10-30 18:56:48 -0300

    Gerando modelo próprio para modificar os Pokemons
    - Classe Pokemon apenas com os atributos necessários
    - Utilização do objeto dessa classe para simplificar o detalhamento dos Pokemons
    - Modelo vinculado ao documento html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,14 @@
-function convertPokemonTypesToLi(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => `<li class='type'>${typeSlot.type.name}</li>`);
-}
-
 function convertPokemonToHtml(pokemon) {
     return `
-        <li class="pokemon">
-            <span class="number">#${pokemon.order}</span>
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
             <div class="detail">
                 <ol class="types">
-                    ${convertPokemonTypesToLi(pokemon.types).join("")}
+                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join("")}
                 </ol>
 
-                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
             </div>
         </li>
     `
