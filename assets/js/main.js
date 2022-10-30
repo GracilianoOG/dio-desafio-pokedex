435cf53f83af71dd92ba307d2d7e70a10c1d309c
Gabriel <gabriel.gracilianomb@gmail.com>
2022-10-30 16:50:48 -0300

    Efetuando requisição para api de Pokemon
    - Efetuando requisição para a api de Pokemon
    - Id adicionado na lista de Pokemons do documento html
    - Retornando um template de Pokemon em html através de uma função
    - Utilizando a função de template de Pokemon para colocá-los dinamicamente no documento html
    - Separando o script principal do script de consumo da api
    - Adicionando o novo arquivo 'poke-api.js' no documento html

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+function convertPokemonToHtml(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById("pokemonList");
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToHtml).join("");
+})
