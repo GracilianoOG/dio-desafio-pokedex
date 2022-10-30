fe84e4d24ce4c93f179e4bde947775adbcc4e581
Gabriel <gabriel.gracilianomb@gmail.com>
2022-10-30 18:22:27 -0300

    Pokemons devidamente detalhados na página
    - Todas as informações estão sendo mostrados de acordo com o Pokemons
    - Nome, ordem, tipos e imagem estão sendo devidamente mostrados de acordo com o Pokemons
    - Duas requisições estão sendo feitas para buscar os detalhes corretos de cada Pokemon
    - Funções auxiliares adicionadas para buscar o Pokemon individualmente e seus respectivos tipos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,18 @@
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class='type'>${typeSlot.type.name}</li>`);
+}
+
 function convertPokemonToHtml(pokemon) {
     return `
         <li class="pokemon">
-            <span class="number">#001</span>
+            <span class="number">#${pokemon.order}</span>
             <span class="name">${pokemon.name}</span>
             <div class="detail">
                 <ol class="types">
-                    <li class="type">grass</li>
-                    <li class="type">poison</li>
+                    ${convertPokemonTypesToLi(pokemon.types).join("")}
                 </ol>
 
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
             </div>
         </li>
     `
@@ -18,5 +21,5 @@ function convertPokemonToHtml(pokemon) {
 const pokemonList = document.getElementById("pokemonList");
 
 pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToHtml).join("");
+    pokemonList.innerHTML = pokemons.map(convertPokemonToHtml).join("");
 })
