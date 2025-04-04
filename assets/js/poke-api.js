7cf3438d37b134666037d968f29a69346b3f182f
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-04 13:52:07 -0300

    Converte getPokemons para função async

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -24,12 +24,12 @@ pokeApi.getPokemonDetail = pokemon => {
     .then(convertPokeApiDetailToPokemon);
 };
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemons = async (offset = 0, limit = 10) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-  return fetch(url)
-    .then(response => response.json())
-    .then(jsonBody => jsonBody.results)
-    .then(pokemons => pokemons.map(pokeApi.getPokemonDetail))
-    .then(detailRequests => Promise.all(detailRequests))
-    .then(pokemonDetails => pokemonDetails);
+  const response = await fetch(url);
+  const jsonBody = await response.json();
+  const pokemons = jsonBody.results;
+  const detailRequests = pokemons.map(pokeApi.getPokemonDetail);
+  const pokemonDetails = Promise.all(detailRequests);
+  return pokemonDetails;
 };
