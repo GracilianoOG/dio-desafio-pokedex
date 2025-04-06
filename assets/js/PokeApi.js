98d8d54bccb107012f4f41e2ddadbc67b7dcfd6b
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-06 11:47:37 -0300

    Adicionado PokeApi
    Essa versão utiliza classe, possibilitando uma escrita mais limpa e clara do código.

---
+++ assets/js/PokeApi.js
@@ -0,0 +1,43 @@
+import { Pokemon } from "./models/Pokemon.js";
+
+class PokeApi {
+  convertPokeApiDetailToPokemon(details) {
+    const { name, id, types, sprites } = details;
+    const typeNames = types.map(typeSlot => typeSlot.type.name);
+    const [type] = typeNames;
+    const photo = sprites.other.dream_world.front_default;
+
+    return new Pokemon(name, id, type, typeNames, photo);
+  }
+
+  async getPokemonDetail(pokemon) {
+    try {
+      const response = await fetch(pokemon.url);
+      if (!response.ok)
+        throw new Error("Erro HTTP! Status: " + response.status);
+      const pokeDetail = await response.json();
+      return this.convertPokeApiDetailToPokemon(pokeDetail);
+    } catch (err) {
+      console.error("Erro ao buscar detalhes do Pokemón: " + err);
+    }
+  }
+
+  async getPokemons(offset = 0, limit = 10) {
+    try {
+      const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+      const response = await fetch(url);
+      if (!response.ok)
+        throw new Error("Erro HTTP! Status: " + response.status);
+      const data = await response.json();
+      const pokemons = data.results;
+      const detailRequests = pokemons.map(this.getPokemonDetail.bind(this));
+      const pokemonDetails = Promise.all(detailRequests);
+
+      return pokemonDetails;
+    } catch (err) {
+      console.error("Erro ao buscar Pokemóns: " + err);
+    }
+  }
+}
+
+export const pokeApi = new PokeApi();
