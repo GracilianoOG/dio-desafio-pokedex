e645e1ec0d194ab27112cf9e7092ccaa3d88fbc5
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-07 22:45:59 -0300

    Agora o próprio card de Pokémon é reaproveitado
    Quando um cartão é clicado, o estilo de seleção é aplicado, centralizando-o na tela e adicionando estilos específicos para mostrar o card de uma forma mais espandida.

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,25 +58,10 @@ pokemonModal.addEventListener("click", e => {
   pokemonModal.classList.toggle("hidden");
 });
 
-pokemonList.addEventListener("click", async e => {
+pokemonList.addEventListener("click", e => {
   const card = e.target.closest(".pokemon");
   if (!card) return;
-
-  const url = `https://pokeapi.co/api/v2/pokemon/${card.dataset.id}`;
-  const name = pokemonModal.querySelector("#modalName");
-  const number = pokemonModal.querySelector("#modalNumber");
-  const photo = pokemonModal.querySelector("#modalPhoto");
-  const photoWrapper = pokemonModal.querySelector("#modalPhotoWrapper");
-  const types = pokemonModal.querySelector("#modalTypes");
-  const pokemon = await pokeApi.getPokemonDetail({ url });
-
-  name.textContent = pokemon.name;
-  number.textContent = `#${pokemon.number}`;
-  photo.src = pokemon.photo;
-  photoWrapper.classList.remove(photoWrapper.classList[1]);
-  photoWrapper.classList.add(pokemon.type);
-
-  pokemonModal.classList.toggle("hidden");
+  card.classList.toggle("selected");
 });
 
 loadPokemonItens(offset, limit);
