fb0a1e8e5465c337eda815a8cb1865019c0d9644
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-10 22:46:07 -0300

    Função para fechar a modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,7 +1,11 @@
 const pokemonModal = document.getElementById("modal");
 
+export const closeModal = () => {
+  pokemonModal.classList.remove("hidden");
+};
+
 export const showModal = (title, message) => {
   pokemonModal.querySelector("#modalTitle").textContent = title;
   pokemonModal.querySelector("#modalDescription").textContent = message;
-  pokemonModal.classList.remove("hidden");
+  closeModal();
 };
