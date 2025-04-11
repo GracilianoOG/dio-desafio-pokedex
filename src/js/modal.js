c40dd5dd43ee6ca8f4c649b4c8678ac2d82f7d80
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-10 21:53:56 -0300

    Função de mostrar modal com título e mensagem

---
+++ src/js/modal.js
@@ -0,0 +1,5 @@
+export const showModal = (title, message) => {
+  pokemonModal.querySelector("#modalTitle").textContent = title;
+  pokemonModal.querySelector("#modalDescription").textContent = message;
+  pokemonModal.classList.remove("hidden");
+};
