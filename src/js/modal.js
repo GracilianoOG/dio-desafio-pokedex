d285b8eb31216281ada0a3023b8b9cc1d8749f8e
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-10 22:07:48 -0300

    Referência do modal adicionada

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,3 +1,5 @@
+const pokemonModal = document.getElementById("modal");
+
 export const showModal = (title, message) => {
   pokemonModal.querySelector("#modalTitle").textContent = title;
   pokemonModal.querySelector("#modalDescription").textContent = message;
