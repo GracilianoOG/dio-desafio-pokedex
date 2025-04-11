fbf8aec10587a4bdbfc0920adf1dfee9ca94466b
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-10 22:50:00 -0300

    Correção na abertura e fechamento da modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,11 +1,11 @@
 const pokemonModal = document.getElementById("modal");
 
 export const closeModal = () => {
-  pokemonModal.classList.remove("hidden");
+  pokemonModal.classList.add("hidden");
 };
 
 export const showModal = (title, message) => {
   pokemonModal.querySelector("#modalTitle").textContent = title;
   pokemonModal.querySelector("#modalDescription").textContent = message;
-  closeModal();
+  pokemonModal.classList.remove("hidden");
 };
