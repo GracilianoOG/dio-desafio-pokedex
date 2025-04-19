10db7ff1212bdabbe81710ed3590fc7dac0d60e8
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-18 21:36:49 -0300

    Add height and weight to Pokemon

--- src/js/models/Pokemon.js
+++ src/js/models/Pokemon.js
@@ -1,9 +1,11 @@
 export class Pokemon {
-  constructor(name, number, type, types, photo) {
+  constructor(name, number, type, types, photo, height, weight) {
     this.name = name;
     this.number = number;
     this.type = type;
     this.types = types;
     this.photo = photo;
+    this.height = height;
+    this.weight = weight;
   }
 }
